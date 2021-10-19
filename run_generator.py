98564c7e50f1aea45e5a2d27227b55e28be0ebdf
hh3123 <84402213+hh3123@users.noreply.github.com>
2021-10-19 17:55:38 +0300

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,12 +1,13 @@
 import secrets, json, re, pathlib, getpass, time, os
+import os as alpha
 
-!curl -LJOk https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
-!unzip -n ngrok-stable-linux-amd64.zip
+alpha.system("curl -LJOk https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip")
+alpha.system("unzip -n ngrok-stable-linux-amd64.zip")
 region = 'eu'
 get_ipython().system_raw("./ngrok http -region {} 8889 &".format(region))
-!sleep 5
-tunnels = !curl -s http://localhost:4040/api/tunnels
+alpha.system("sleep 5")
+tunnels = alpha.system("curl -s http://localhost:4040/api/tunnels")
 url = json.loads(tunnels[0])["tunnels"][1]["public_url"]
-!sleep 1
+alpha.system("sleep 1")
 print(url)
-!jupyter notebook --ip 0.0.0.0 --port 8889 --allow-root
+alpha.system("jupyter notebook --ip 0.0.0.0 --port 8889 --allow-root")
